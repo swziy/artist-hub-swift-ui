27ee5c323d9c532b8d62103a60026d169e4982bc
Szymon Witkowski <witkowski.sm@gmail.com>
2022-04-02 18:37:44 +0200

    Add generated Xcode project

---
+++ ArtistHub/ArtistHub/ArtistHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistHubApp.swift
+//  ArtistHub
+//
+//  Created by Szymon Witkowski on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArtistHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
