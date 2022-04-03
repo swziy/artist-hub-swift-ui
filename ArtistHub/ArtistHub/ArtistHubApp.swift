418fd9ea75def4b08a05bb24b1ee91348f07102d
Szymon Witkowski <witkowski.sm@gmail.com>
2022-04-03 16:52:22 +0200

    Configure navigation bar

--- ArtistHub/ArtistHub/ArtistHubApp.swift
+++ ArtistHub/ArtistHub/ArtistHubApp.swift
@@ -1,10 +1,40 @@
+import ComposableArchitecture
 import SwiftUI
+
+struct AppState {
+
+}
+
+enum AppAction {
+
+}
+
+struct AppEnvironment {
+
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+
+    }
+}
 
 @main
 struct ArtistHubApp: App {
+
+    init() {
+        NavigationBarAppearanceConfigurator().configure()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ListView()
+            ListView(
+                store: Store(
+                    initialState: AppState(),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                )
+            )
         }
     }
 }
--- ArtistHub/ArtistHub/ListView.swift
+++ ArtistHub/ArtistHub/ListView.swift
@@ -1,14 +1,27 @@
+import ComposableArchitecture
 import SwiftUI
 
 struct ListView: View {
+    let store: Store<AppState, AppAction>
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("Hello")
+            }
+            .navigationBarTitle("Artist Hub")
+        }
     }
 }
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView(
+            store: Store(
+                initialState: AppState(),
+                reducer: appReducer,
+                environment: AppEnvironment()
+            )
+        )
     }
 }
